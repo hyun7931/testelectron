58f211696b8854197fedc472709812a1745b7609
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-01-19 02:50:16 +0900

    feat: connect add URL, open btn

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+//renderer.js
+const { ipcRenderer } = require('electron');
 const panel = document.getElementById("widget");
 const wrapper = document.getElementById("wrapper");
 const toggleBtn = document.getElementById("toggleBtn");
@@ -5,8 +7,16 @@ const collapsedIcon = document.getElementById("collapsedIcon");
 
 let pinned = false; // 고정상태
 
+//임시 확인용 : 노란색 버튼 누르면 -> 강의 교안
+const btnYellow = document.querySelector('.yellow');
+btnYellow.addEventListener('click', () => {
+  ipcRenderer.send('trigger-chrome', 'https://baceru.vercel.app');
+});
+
 // + 버튼 클릭
-toggleBtn.addEventListener("click", () => {});
+toggleBtn.addEventListener("click", () => {
+  ipcRenderer.send('open-popup');
+});
 
 /**
  * 아이콘 클릭 규칙
